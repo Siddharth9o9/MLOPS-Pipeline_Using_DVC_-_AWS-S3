9e3b39e111acd1e1f4192eeb88d0b8748464c173
Siddharth <sid.8960266720@gmail.com>
2026-02-04 09:49:39 +0530

    Pipeline ready with all components

---
+++ src/feature_engineering.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+import logging
+
+
+# Creating logs directory
+log_dir = 'logs'
+os.makedirs(log_dir,exist_ok=True)
+
+# Logging configuration
+logger = logging.getLogger('feature_engineering')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir,'feature_engineering.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
+def load_data(data_url: str) -> pd.DataFrame:
+    '''
+    Load data from CSV file.
+
+    '''
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug('Data loaded from %s',data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occured while loading the data: %s', e)
+        raise
+    
+    
+def apply_tfidf(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
+    '''
+    Applying tfidf to convert text into vectors
+    
+    '''
+    try:
+        vectorizer = TfidfVectorizer(max_features=max_features)
+        
+        X_train = train_data['text'].fillna('').values
+        y_train = train_data['target'].values
+        X_test = train_data['text'].fillna('').values
+        y_test = train_data['target'].values
+        
+        X_train_bow = vectorizer.fit_transform(X_train)
+        X_test_bow = vectorizer.transform(X_test)
+        
+        train_df = pd.DataFrame(X_train_bow.toarray())
+        train_df['label'] = y_train
+        
+        test_df = pd.DataFrame(X_test_bow.toarray())
+        test_df['label'] = y_test
+        
+        logger.debug('TFIDF applied and data transformed')
+        return train_df, test_df
+    
+    except Exception as e:
+        logger.error('Error during Bag of words transformation %s',e)
+        raise
+    
+def save_data(df: pd.DataFrame, file_path: str) -> None:
+    '''Save the df to a CSV file'''
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        df.to_csv(file_path, index=False)
+        logger.debug('Data saved to %s', file_path)
+    except Exception as e:
+        logger.error('Unexpected error occured while saving the data: %s', e)
+        raise
+    
+def main():
+    try:
+        max_features = 50
+        
+        train_data = load_data('./data/interim/train_processed.csv')
+        test_data = load_data('./data/interim/test_processed.csv')
+        
+        train_df, test_df  = apply_tfidf(train_data, test_data, max_features)
+        
+        save_data(train_df, os.path.join("./data","processed","train_tfidf.csv"))
+        save_data(test_df, os.path.join("./data","processed","test_tfidf.csv"))
+    except Exception as e:
+        logger.error('Failed to complete the feature engineering process: %s', e)
+        print(f'Error: {e}')
+    
+if __name__ == '__main__':
+    main()
+
+        
---
+++ src/utils/logger.py
@@ -0,0 +1,6 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+def get_logger(module_name):
+    pass
